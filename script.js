95d94eb0d689c4286bd8b3102f5195174075df19
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2025-10-02 21:46:09 +0300

    refactor: use `getElementsByClassName`

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.nav__burger');
-    const navList = document.querySelector('.nav__list');
+    const [burger] = document.getElementsByClassName('nav__burger');
+    const [navList] = document.getElementsByClassName('nav__list');
 
     burger.addEventListener('click', () => {
         navList.classList.toggle('nav__list--open');
